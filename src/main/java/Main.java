3408f565e8aac1720956b38efda53fd5d34bb97d
shahryar <62142539+shahryar-karimi@users.noreply.github.com>
2021-05-23 13:53:22 +0430

    deleting useless classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         CommandProcessor commandProcessor = new CommandProcessor();
         commandProcessor.loadManager();
         commandProcessor.run();

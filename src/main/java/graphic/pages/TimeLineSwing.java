d01bc23d3fb12fb19b53c3682c4a31e2d9e84236
shahryar <62142539+shahryar-karimi@users.noreply.github.com>
2021-07-06 20:23:27 +0430

    update

--- src/main/java/graphic/pages/TimeLineSwing.java
+++ src/main/java/graphic/pages/TimeLineSwing.java
@@ -7,12 +7,10 @@ import graphic.pages.personalPage.PersonalPageSwing;
 import logic.Account;
 import logic.Tweet;
 import logic.pages.TimeLinePage;
-import logic.pages.personal.PersonalPage;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.LinkedList;
